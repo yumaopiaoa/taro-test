3b899b0538fa698be6d2c4bacf224f58e498e6f9
liubin <liubin@houkeyun.com>
2022-11-07 12:01:39 +0800

    taro 测试代码

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <text @click="fn">我是一段文字</text>
+  </view>
+</template>
+
+<script>
+import "./index.scss";
+
+export default {
+  setup() {
+    const fn = () => {
+      console.log("666");
+    };
+    return {
+      fn,
+    };
+  },
+};
+</script>
---
+++ src/pages/index2/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <view class="index">
+    index2
+  </view>
+</template>
+
+<script>
+
+export default {
+  setup() {
+    return {
+    };
+  },
+};
+</script>
